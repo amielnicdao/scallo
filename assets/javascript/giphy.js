debd348ce6bfa57f2f591d6800ece86712eb42f7
anicdao <amielnicdao@gmail.com>
2019-10-24 15:19:44 -0400

    added ajax call and on click listeners

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -6,30 +6,59 @@
 //append search results to html file
 //create for loop for prepopulated topics (travel) buttons
 
-// $(document).ready(function() {
+$(document).ready(function () {
+
+    var topics = ["London", "Paris", "Phuket", "Bali", "Tokyo", "Singapore"];
 
 
+    function displayInitialTopics() {
+        $("#travel-buttons").empty();
 
-// }); needed?
+        for (var i = 0; i < topics.length; i++) {
 
-var topics = ["London", "Paris", "Phuket", "Bali", "Tokyo", "Singapore"];
+            var initialTopics = $("<button>");
+            initialTopics.addClass("travel-btn");
+            initialTopics.attr("data-name", topics[i]);
+            initialTopics.text(topics[i]);
+            $("#travel-buttons").append(initialTopics);
+        }
+    }
 
-// function displayTopics() {
-//     var topic = $(this).attr("data-name");
-//     var queryURL = https://api.giphy.com/v1/gifs/search?api_key=eFzG2a2AQjNm9AZTaKy8K34s5WGOC5CX
-// }
+    function displayTopics() {
+        var travel = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + travel + "&api_key=eFzG2a2AQjNm9AZTaKy8K34s5WGOC5CX&limit=10";
 
-function renderTopics() {
-    $("#travel-button").empty();
+        $.ajax({
 
-    for (var i = 0; i < topics.length; i++) {
+            url: queryURL,
+            method: "GET"
 
-        var initialTopics = $("<button>");
-        initialTopics.addClass("movie-btn");
-        initialTopics.attr("data-name", topics[i]);
-        initialTopics.text(topics[i]);
-        $("#travel-button").append(initialTopics);
-}
+        }).then(function (response) {
 
+            var topicsDiv = $("<div class='topics'>");
+            var rating = response.rating; //.data
+            var pRating = $("<p>").text("Rating: " + rating); 
+            topicsDiv.append(pRating);
+            $("#travel-view").prepend(topicsDiv);
+            //add image
 
-}
+        })
+    }
+
+    $("#add-place").on("click", function(event) {
+        event.preventDefault();
+
+        var newTopics = $("#travel-input").val().trim();
+
+        topics.push(newTopics);
+
+        displayInitialTopics();
+    });
+
+    $(document).on("click", ".travel-btn", displayTopics);
+
+        displayInitialTopics();
+
+});
+
+// add function for gif state
